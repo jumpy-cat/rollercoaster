fc07a4b291f3679a3f1198657afc66b0433da470
Edward Zhang <edward2025245@concordiashanghai.org>
2024-12-29 14:36:29 +0800

    document future method

--- num_opt/src/physics/mod.rs
+++ num_opt/src/physics/mod.rs
@@ -1,12 +1,12 @@
 //! Physics solver and cost function
 
-use godot::global::{godot_print, godot_warn};
 use linalg::{scaler_projection, vector_projection, MyQuaternion, MyVector3, Silence};
 
 use crate::{hermite, my_float::MyFloat};
 
 pub mod legacy;
 pub mod linalg;
+mod solver;
 
 /// Possible ways for the physics system to take a step  
 /// Steps are in `u`, the (0,1) parameterization of hermite curves
@@ -37,32 +37,6 @@ macro_rules! float {
 }
 
 pub(crate) use float;
-
-fn find_root_bisection<T: MyFloat>(a: T, b: T, f: impl Fn(&T) -> T, epsilon: f64) -> Option<T> {
-    let mut a = a;
-    let mut b = b;
-    if a > b {
-        std::mem::swap(&mut a, &mut b);
-    }
-    let mut fa = f(&a);
-    let fb = f(&b);
-    if (fa.clone() * fb) > 0.0 {
-        // they have the same sign, so there is no root in this interval
-        return None;
-    }
-    while (b.clone() - a.clone()).abs() > epsilon {
-        let m = (a.clone() + b.clone()) / T::from_f64(2.0);
-        let fm = f(&m);
-        if (fa.clone() * fm.clone()) < 0.0 {
-            b = m;
-            //fb = fm;
-        } else {
-            a = m;
-            fa = fm;
-        }
-    }
-    Some((a + b) / T::from_f64(2.0))
-}
 
 /// Physics solver v3
 /// ### Overview
@@ -113,7 +87,6 @@ const MAX_CURVE_ANGLE: f64 = 0.0001;
 const MIN_DELTA_X: f64 = 0.0001;
 const MAX_TORQUE: f64 = 10.0;
 const ALLOWED_ANGULAR_WIGGLE: f64 = 0.01;
-
 
 impl<T: MyFloat> PhysicsStateV3<T> {
     /// Initialize the physics state: `m` mass, `g` Gravity vector,
@@ -170,8 +143,9 @@ impl<T: MyFloat> PhysicsStateV3<T> {
         s
     }
 
-    fn calc_new_u_from_delta_t(
-        &mut self,
+    #[deprecated]
+    fn calc_new_u_from_delta_t_old(
+        &self,
         step: &T,
         init_bracket_amount: T,
         curve: &hermite::Spline<T>,
@@ -186,13 +160,10 @@ impl<T: MyFloat> PhysicsStateV3<T> {
         if u_upper_bound == curve.max_u() {
             return None;
         }
-        let roots = find_root_bisection(
+        let roots = solver::find_root_bisection(
             u_lower_bound,
             u_upper_bound,
             |u| {
-                /*let v1 = new_pos.clone() - curve.curve_at(u).unwrap();
-                let v2 = curve.curve_1st_derivative_at(u).unwrap();
-                godot_print!("v1: {:.3?} v2: {:.3?}", v1, v2);*/
                 scaler_projection(
                     new_pos.clone() - curve.curve_at(u).unwrap(),
                     curve.curve_1st_derivative_at(u).unwrap(),
@@ -202,45 +173,111 @@ impl<T: MyFloat> PhysicsStateV3<T> {
         );
         match roots {
             Some(root) => Some(root),
-            None => self.calc_new_u_from_delta_t(step, init_bracket_amount * 2.0, curve),
+            None => self.calc_new_u_from_delta_t_old(step, init_bracket_amount * 2.0, curve),
         }
     }
 
-    /// ### Step Sizes
-    /// `StepBehavior::Constant`: Use a fixed step size step.
-    /// `StepBehavior::Distance`: Adjust delta_u to keep the traveled arc length constant
-    /// `StepBehavior::Time`: Adjust delta_u based on both velocity and arc length.
-    /// if `dsdu` is zero, a fallback step size is used
+    fn calc_new_u_from_delta_t(
+        &self,
+        step: &T,
+        init_bracket_amount: T,
+        curve: &hermite::Spline<T>,
+    ) -> Option<T> {
+        let upper_bound = self.u.clone() + init_bracket_amount.clone();
+        if upper_bound > curve.max_u() {
+            return None;
+        }
+
+        match solver::find_minimum_golden_section(
+            self.u.clone(),
+            upper_bound,
+            // (||r(u_next) + N * o - x_curr + a * dt^2|| - ||v_curr|| * dt)^2
+            |u| {
+                ((curve.curve_at(&u).unwrap() + self.hl_normal.clone() * self.o.clone()
+                    - self.x.clone()
+                    + self.g.clone() * step.clone().pow(2))
+                .magnitude()
+                    - self.v.magnitude() * step.clone())
+                .pow(2)
+            },
+            1e-14f64,
+        ) {
+            Some((u, _fu)) => Some(u),
+            None => None,
+        }
+    }
+
+    /// Steps forward in time by `step`, may choose to perform smaller step(s)
+    ///
+    /// ### Implementation Details
+    /// The physics is based off semi-implicit euler integration, with
+    /// corrective forces choosen to keep the position perfectly correct at all
+    /// times.
+    ///
+    /// The basic rule would be:  
+    /// `v_next = v_curr + a * dt`  
+    /// `x_next = x_curr + v_next * dt`
+    ///
+    /// Acceleration would be from gravity and normal force. The normal force
+    /// can be expressed as a rotation of `v_curr`, this ensures `F_N` doesn't
+    /// add any energy to the system.
+    ///
+    /// Thus, we rewrite the update rules as:  
+    /// `v_next = rot(v_curr, R) + a * dt`  
+    /// `x_next = x_curr + v_next * dt`
+    ///
+    /// Give a target `x_tar`, we subsitute `x_next = x_tar`:  
+    /// `x_tar = x_curr + v_next * dt`  
+    /// `x_tar = x_curr + (rot(v_curr, R) + a * dt) * dt`  
+    /// `x_tar = x_curr + rot(v_curr, R) * dt + a * dt^2`  
+    /// `x_tar - (x_curr + a * dt^2) = rot(v_curr, R) * dt`
+    ///
+    /// Considering the rotation as creating a sphere:  
+    /// `||x_tar - (x_curr + a * dt^2)|| = ||rot(v_curr, R) * dt||`  
+    /// `||x_tar - (x_curr + a * dt^2)|| = ||v_curr|| * dt`  
+    /// `||x_tar - (x_curr + a * dt^2)|| - ||v_curr|| * dt = 0`  
+    /// `(||x_tar - (x_curr + a * dt^2)|| - ||v_curr|| * dt)^2 = 0`
+    ///
+    /// Which can be solved for `x_tar`:  
+    /// `x_tar = r(u_next) + N * o`  
+    /// `(||r(u_next) + N * o - x_curr + a * dt^2|| - ||v_curr|| * dt)^2 = 0`
+    ///
+    /// Note that this may have no solution, corresponding to a target position
+    /// that can't be reached given the current velocity. (Most notably when
+    /// velocity is zero, also possible if `N` is discontinuous).\
+    /// In that case, we find the minimum, which corresponds to the closest
+    /// point on the curve to the future position given velocity "trying its
+    /// best". Then apply the rules knowing we will diverge slightly
+    /// from the curve.
+    ///
+    /// `rot(v_curr, R) = x_tar / dt - x_curr / dt - a * dt`
+    ///
     pub fn step(
         &mut self,
         step: T,
         curve: &hermite::Spline<T>,
         behavior: StepBehavior,
     ) -> Option<()> {
-        if self.torque_exceeded {
-            //return None;
-        }
-
         self.total_t_ = self.total_t_.clone() + step.clone();
         let new_delta_t = step.clone();
+
+        /*let new_u = self
+        .calc_new_u_from_delta_t_old(&step, T::from_f64(0.00001), curve)
+        .unwrap();*/
         let new_u = self
             .calc_new_u_from_delta_t(&step, T::from_f64(0.00001), curve)
             .unwrap();
         self.delta_u_ = new_u.clone() - self.u.clone();
 
-        // Advance the parametric value u by delta_u and calculate the new position along the curve.
-        // The displacement vector delta_x is the difference between the new position and the current position.
-        //let new_u = self.u + self.delta_u_;
-
         let kappa = curve.curve_kappa_at(&new_u).unwrap();
+        #[allow(non_snake_case)]
         let N = curve.curve_normal_at(&new_u).unwrap();
         // a = v^2 / r
         // r = 1 / kappa
         // a = kappa * v^2
         let accel = N * kappa * self.hl_vel.clone().magnitude().pow(2);
 
-        self.ag_ = /*self.hl_accel.clone()*/accel.clone() - self.g.clone();
-        //self.ag_ = self.hl_accel.clone() - self.g.clone();
+        self.ag_ = accel.clone() - self.g.clone();
         self.target_hl_normal_ = if self.torque_exceeded {
             self.hl_normal.clone()
         } else {
@@ -250,29 +287,18 @@ impl<T: MyFloat> PhysicsStateV3<T> {
             .normalize();
             (tmp.clone() - vector_projection(tmp.clone(), ortho_to.clone())).normalize()
         };
+        
         self.delta_x_target_ = curve.curve_at(&new_u).unwrap()
             - self.target_hl_normal_.clone() * self.o.clone()
             - self.x.clone();
 
-        /*let step_too_big = {
-            self.v.angle(&self.delta_x_target_) > MAX_CURVE_ANGLE
-                //&& self.delta_u_ != FALLBACK_STEP
-                && step > MIN_STEP
-                //&& self.delta_x_target_.magnitude() > MIN_DELTA_X
-        };*/
         let step_too_big = accel.magnitude() * step.clone() > 0.00001 && step > MIN_STEP;
         if step_too_big {
-            self.step(step.clone() * T::from_f64(0.5), curve, behavior).unwrap();
+            self.step(step.clone() * T::from_f64(0.5), curve, behavior)
+                .unwrap();
             self.step(step * T::from_f64(0.5), curve, behavior).unwrap();
             return Some(());
         }
-        //self.delta_t_ = self.calc_delta_t_from_delta_u(step).unwrap();
-
-        // Normal Force: Derived from displacement, velocity, and gravity.
-        // v: semi implicit Euler integration
-        // Position(x): Advance based on velocity
-        // u: A Advances to the next point.
-        //self.F_N_ = na::Vector3::zeros();
 
         self.F_N_ = (self.delta_x_target_.clone() / new_delta_t.clone().pow(2)
             - self.v.clone() / new_delta_t.clone()
@@ -280,10 +306,8 @@ impl<T: MyFloat> PhysicsStateV3<T> {
             * self.m.clone();
         self.F_ = self.F_N_.clone() + self.g.clone() * self.m.clone();
         // hl values
-        let new_hl_vel =
-            (curve.curve_at(&new_u)? - curve.curve_at(&self.u)?) / new_delta_t.clone();
+        let new_hl_vel = (curve.curve_at(&new_u)? - curve.curve_at(&self.u)?) / new_delta_t.clone();
         let new_hl_accel = (new_hl_vel.clone() - self.hl_vel.clone()) / (new_delta_t.clone());
-        // let new_hl_accel = (new_hl_vel.clone() - self.hl_vel.clone()) * T::from_f64(2.0) / (new_delta_t.clone() + self.delta_t_.clone());
         // rotation
         let cross = self.hl_normal.cross(&self.target_hl_normal_).normalize();
         self.delta_hl_normal_target_ =
@@ -296,21 +320,6 @@ impl<T: MyFloat> PhysicsStateV3<T> {
                 - self.w.clone() / new_delta_t.clone())
                 * self.I.clone();
         }
-        /*if !self.torque_exceeded
-            && self.torque_.magnitude() > MAX_TORQUE
-            && self.delta_hl_normal_target_.magnitude() > ALLOWED_ANGULAR_WIGGLE
-        {
-            self.torque_exceeded = true;
-            //let delta_y = self.hl_normal.clone() * self.o.clone();
-            let curr_energy = self.energy();
-            self.o = T::from_f64(0.0);
-            self.x = curve.curve_at(&self.u).unwrap();
-            self.v = self.v.clone().normalize()
-                * ((curr_energy - self.potential_energy()).max(&T::from_f64(0.0)) * 2.0 / self.m.clone()).sqrt();
-            // REMEMBER TO REMOVE
-            std::thread::sleep(std::time::Duration::from_millis(200));
-            return self.step(step, curve, behavior);
-        }*/
 
         // semi-implicit euler update rule
         self.v = self.v.clone() + self.F_.clone() / self.m.clone() * new_delta_t.clone();
---
+++ num_opt/src/physics/solver.rs
@@ -0,0 +1,83 @@
+use crate::my_float::MyFloat;
+
+pub fn find_root_bisection<T: MyFloat>(a: T, b: T, f: impl Fn(&T) -> T, epsilon: f64) -> Option<T> {
+    let mut a = a;
+    let mut b = b;
+    if a > b {
+        std::mem::swap(&mut a, &mut b);
+    }
+    let mut fa = f(&a);
+    let fb = f(&b);
+    if (fa.clone() * fb) > 0.0 {
+        // they have the same sign, so there is no root in this interval
+        return None;
+    }
+    while (b.clone() - a.clone()).abs() > epsilon {
+        let m = (a.clone() + b.clone()) / T::from_f64(2.0);
+        let fm = f(&m);
+        if (fa.clone() * fm.clone()) < 0.0 {
+            b = m;
+            //fb = fm;
+        } else {
+            a = m;
+            fa = fm;
+        }
+    }
+    Some((a + b) / T::from_f64(2.0))
+}
+
+#[test]
+fn test_find_minimum_golden_section() {
+    let f = |x: &f64| (x - 0.5) * (x - 0.5) + 1.0;
+    let r = find_minimum_golden_section(0.0, 1.0, f, 1e-6);
+    assert!(r.is_some());
+    assert!((r.unwrap().0 - 0.5).abs() < 1e-6);
+    assert!((r.unwrap().1 - 1.0).abs() < 1e-6);
+}
+
+#[test]
+fn test_find_minimum_golden_section_none() {
+    let f = |x: &f64| (x - 0.5) * (x - 0.5) + 1.0;
+    let r = find_minimum_golden_section(1.0, 2.0, f, 1e-6);
+    assert!(r.is_none());
+}
+
+pub fn find_minimum_golden_section<T: MyFloat>(
+    a_: T,
+    b_: T,
+    f: impl Fn(&T) -> T,
+    epsilon: f64,
+) -> Option<(T, T)> {
+    let r = T::from_f64((3.0 - 5.0_f64.sqrt()) / 2.0);
+
+    let mut a = a_.clone();
+    let mut b = b_.clone();
+
+    let mut c = a.clone() + r.clone() * (b.clone() - a.clone());
+    let mut d = b.clone() - r.clone() * (b.clone() - a.clone());
+
+    // a -------- c --- d -------- b
+
+    while (b.clone() - a.clone()).abs() > epsilon {
+        let fc = f(&c);
+        let fd = f(&d);
+        if fc < fd {
+            // minimum is between a and d
+            b = d;
+            d = c;
+            c = a.clone() + r.clone() * (b.clone() - a.clone());
+        } else {
+            // minimum is between c and b
+            a = c;
+            c = d;
+            d = b.clone() - r.clone() * (b.clone() - a.clone());
+        }
+    }
+    let potential = (b.clone() + a.clone()) / T::from_f64(2.0);
+    if potential > a_ + T::from_f64(epsilon) && potential < b_ - T::from_f64(epsilon) {
+        Some((potential.clone(), f(&potential)))
+    } else {
+        // minimum is a boundary
+        None
+    }
+}
