2fa41b85e6c1a69a7db2a121b599d65ba93e319f
Edward Zhang <edward2025245@concordiashanghai.org>
2025-01-02 12:24:12 +0800

    colored logs

--- gd/src/lib.rs
+++ gd/src/lib.rs
@@ -14,7 +14,11 @@ unsafe impl ExtensionLibrary for MyExtension {
         if level == InitLevel::Scene {
             log::set_logger(&LOGGER).unwrap();
             log::set_max_level(log::LevelFilter::Trace);
-            log::error!("Hello Godot!");
+            log::error!("Hi!");
+            log::warn!("Hi!");
+            log::info!("Hi!");
+            log::debug!("Hi!");
+            log::trace!("Hi!");
             godot_print!(">!!!!");
         }
     }
@@ -29,7 +33,14 @@ impl log::Log for GodotLogger {
 
     fn log(&self, record: &log::Record) {
         //godot_print_rich!()
-        godot_print!("{} - {}", record.level(), record.args())
+        let color = match record.level() {
+            log::Level::Error => "red",
+            log::Level::Warn => "yellow",
+            log::Level::Info => "cyan",
+            log::Level::Debug => "magenta",
+            log::Level::Trace => "pink",
+        };
+        godot_print_rich!("[color={color}]{}[/color] - {}", record.level(), record.args());
     }
 
     fn flush(&self) {
